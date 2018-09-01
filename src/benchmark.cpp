c0fbba99f1efe3a4cafcb51751cd7256a4dabfa8
Tim Quelch <tim@quelch.name>
2018-09-01 16:03:47 +1000

    Fix benchmark

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -5,13 +5,19 @@
 using namespace seqslam;
 
 void cpuDifferenceMatrix(benchmark::State& state) {
-    auto dataDir = std::filesystem::path{"../datasets/nordland_trimmed"};
-    auto referenceImages = convertToEigen(contrastEnhancement(readImages(dataDir / "summer"), 20));
-    auto queryImages = convertToEigen(contrastEnhancement(readImages(dataDir / "winter"), 20));
+    const auto dataDir = std::filesystem::path{"../datasets/nordland_trimmed"};
+    const auto referenceImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "summer"), 20));
+    const auto queryImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "winter"), 20));
 
     for (auto _ : state) {
         auto diffMatrix = cpu::generateDiffMx(referenceImages, queryImages);
+        benchmark::DoNotOptimize(diffMatrix.get());
+        benchmark::ClobberMemory();
     }
+    state.SetItemsProcessed(state.iterations() * referenceImages.size() * queryImages.size() *
+                            nRows * nCols);
 }
 BENCHMARK(cpuDifferenceMatrix)->Unit(benchmark::kMillisecond);
 
