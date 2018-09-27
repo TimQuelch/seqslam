6520d8d97b91032c3b6d1e1ef06aeff424b3d4bd
Tim Quelch <tim@quelch.name>
2018-09-27 00:09:21 -0700

    Add benchmarks for different granularities of calling opencl diffmx

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -19,10 +19,81 @@ void cpuDifferenceMatrix(benchmark::State& state) {
     state.SetItemsProcessed(state.iterations() * referenceImages.size() * queryImages.size() *
                             nRows * nCols);
 }
-BENCHMARK(cpuDifferenceMatrix)
+// BENCHMARK(cpuDifferenceMatrix)
+//    ->Unit(benchmark::kMillisecond)
+//    ->RangeMultiplier(2)
+//    ->Range(1, 256)
+//    ->MinTime(1);
+
+void gpuDifferenceMatrix(benchmark::State& state) {
+    auto const dataDir = std::filesystem::path{"../datasets/nordland_trimmed_resized"};
+    auto const referenceImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "summer"), 20));
+    auto const queryImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "winter"), 20));
+
+    for (auto _ : state) {
+        auto diffMatrix = opencl::generateDiffMx(referenceImages, queryImages, state.range(0));
+        benchmark::DoNotOptimize(diffMatrix.get());
+        benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(state.iterations() * referenceImages.size() * queryImages.size() *
+                            nRows * nCols);
+}
+BENCHMARK(gpuDifferenceMatrix)
     ->Unit(benchmark::kMillisecond)
     ->RangeMultiplier(2)
-    ->Range(1, 512)
-    ->MinTime(15);
+    ->Range(1, 4)
+    ->MinTime(1);
+
+void gpuDifferenceMatrixNoContext(benchmark::State& state) {
+    auto const dataDir = std::filesystem::path{"../datasets/nordland_trimmed_resized"};
+    auto const referenceImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "summer"), 20));
+    auto const queryImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "winter"), 20));
+
+    auto context = opencl::createDiffMxContext();
+
+    for (auto _ : state) {
+        auto diffMatrix =
+            opencl::generateDiffMx(context, referenceImages, queryImages, state.range(0));
+        benchmark::DoNotOptimize(diffMatrix.get());
+        benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(state.iterations() * referenceImages.size() * queryImages.size() *
+                            nRows * nCols);
+}
+BENCHMARK(gpuDifferenceMatrixNoContext)
+    ->Unit(benchmark::kMillisecond)
+    ->RangeMultiplier(2)
+    ->Range(1, 4)
+    ->MinTime(1);
+
+void gpuDifferenceMatrixNoCopy(benchmark::State& state) {
+    auto const dataDir = std::filesystem::path{"../datasets/nordland_trimmed_resized"};
+    auto const referenceImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "summer"), 20));
+    auto const queryImages =
+        convertToEigen(contrastEnhancement(readImages(dataDir / "winter"), 20));
+
+    auto context = opencl::createDiffMxContext();
+    auto bufs = opencl::createBuffers(context, referenceImages.size(), queryImages.size());
+    opencl::writeArgs(context, bufs, referenceImages, queryImages, state.range(0));
+
+    for (auto _ : state) {
+        auto diffMatrix = opencl::generateDiffMx(
+            context, bufs.diffMx, referenceImages.size(), queryImages.size(), state.range(0));
+        benchmark::DoNotOptimize(diffMatrix.get());
+        benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(state.iterations() * referenceImages.size() * queryImages.size() *
+                            nRows * nCols);
+}
+BENCHMARK(gpuDifferenceMatrixNoCopy)
+    ->Unit(benchmark::kMillisecond)
+    ->RangeMultiplier(2)
+    ->Range(1, 4)
+    ->MinTime(1);
 
 BENCHMARK_MAIN();
