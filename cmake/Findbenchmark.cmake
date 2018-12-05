5fc1647cce258c0f96c7d9bfabb06a3e439b922c
Tim Quelch <tim@quelch.name>
2018-12-05 11:30:17 +1000

    Re-add google benchmark fetch content script

---
+++ cmake/Findbenchmark.cmake
@@ -0,0 +1,16 @@
+# Finds the Google Benchmark library
+
+include(FetchContent)
+
+FetchContent_Declare(
+    benchmark
+    GIT_REPOSITORY https://github.com/google/benchmark.git
+    GIT_TAG        v1.4.1)
+
+FetchContent_GetProperties(benchmark)
+if (NOT benchmark_POPULATED)
+    FetchContent_Populate(benchmark)
+    set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Enable testing" FORCE)
+    add_subdirectory(${benchmark_SOURCE_DIR} ${benchmark_BINARY_DIR})
+    add_library(benchmark::benchmark ALIAS benchmark)
+endif()
--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,6 @@ from conans import ConanFile
 
 class SeqslamBenchmarkConan(ConanFile):
     requires = ("fmt/5.2.1@bincrafters/stable",
-                "google-benchmark/1.4.1@timquelch/stable",
                 "opencv/3.4.3@conan/stable",
                 "eigen/3.3.5@conan/stable")
     default_options = {"opencv:openexr": False}
