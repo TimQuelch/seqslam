6c91ade39c4e3c4857f297114debbce939c321df
Tim Quelch <tim@quelch.name>
2019-04-14 15:04:15 +1000

    Update demo and PR scripts to process varying a number of configs

--- scripts/process-pr.py
+++ scripts/process-pr.py
@@ -9,28 +9,37 @@ argparser = argparse.ArgumentParser(description="Process PR curves")
 argparser.add_argument('-s', '--show', action='store_true', help='Display figures as windows')
 argparser.add_argument('-w', '--write', action='store_true', help='Write figures to files')
 
-datafile = 'pr.json'
-fileprefix = 'pr'
+vals = [5, 10, 15, 20, 25, 30]
+datafiles = [('window', vals), ('seqlength', vals), ('ntraj', vals)]
+prefix = 'pr'
+ext = '.json'
 
 def main(args):
-    with open(datafile) as b:
-        raw = json.load(b)
-
     figs = []
 
-    d = pd.DataFrame(raw["data"])
-    print(d)
+    for parameter, values in datafiles:
+        ds = []
+        for val in values:
+            with open(prefix + '-' + parameter + '-' + str(val) + '.json') as b:
+                raw = json.load(b)
 
-    fig, ax = plt.subplots()
-    ax = d.plot(x="Recall", y="Precision", ax=ax)
-    figs.append((fig, 'pr'))
+            ds.append(pd.DataFrame(raw["data"]).set_index('Recall'))
+
+        d = pd.concat(ds, keys=values, names=[parameter])
+        d = d.reset_index()
+        d = d.set_index('Recall')
+
+        fig, ax = plt.subplots()
+        d.groupby(parameter)['Precision'].plot(style='-', ax=ax, legend=True)
+        ax.set_ylabel('Precision')
+        figs.append((fig, prefix + '-' + parameter))
 
     if args.show:
         plt.show()
 
     if args.write:
         for fig, name in figs:
-            fig.savefig(fileprefix + '-' + name + '.pdf')
+            fig.savefig(prefix + '-' + name + '.pdf')
 
 if __name__ == '__main__':
     args = argparser.parse_args()
