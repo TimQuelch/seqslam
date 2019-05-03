fdc81b75c5c55424b0438f7d2df4580a2af4e058
Tim Quelch <tim@quelch.name>
2019-05-03 11:29:55 +1000

    Update dependency versions

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,9 @@ from conans import ConanFile
 class SeqslamBenchmarkConan(ConanFile):
     requires = ("fmt/5.3.0@bincrafters/stable",
                 "opencv/3.4.5@conan/stable",
-                "eigen/3.3.5@conan/stable",
-                "catch2/2.5.0@bincrafters/stable",
-                "jsonformoderncpp/3.5.0@vthiery/stable")
+                "eigen/3.3.7@conan/stable",
+                "Catch2/2.7.1@catchorg/stable",
+                "jsonformoderncpp/3.6.1@vthiery/stable")
     default_options = {"opencv:openexr": False,
                        "opencv:jasper": False}
     generators = ("cmake", "cmake_paths")
